3f420d07973ed0dcc36f9d5f1a219967389642e9
THOMAS HARDING <hardingt604@gmail.com>
2022-04-27 15:26:06 -0600

    added vehiclelist although i feel like it should be in its own div

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import './vehicleList.css';
+import Vehicle from './Vehicle';
 
-export default function VehicleList(prop) {
+export default function vehicleList({ vehicles }) {
   return (
-    <div className="traffic">
-      {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
-      {/* map through the vehicles array in props */}
-      {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
-    </div>
+      vehicles.map((vehicle) => 
+        <Vehicle
+        key={vehicle.name}
+        vehicle={vehicle} />
+      )
   );
 }
